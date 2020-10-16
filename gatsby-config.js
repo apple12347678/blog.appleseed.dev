fedafa192b79f4b65c530371d65cc5d4ee855e77
appleseed <apple12347678@gmail.com>
2020-10-16 23:46:50 +0900

    feat(ts): setup typescript

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,10 +1,13 @@
-/**
- * Configure your Gatsby site with this file.
- *
- * See: https://www.gatsbyjs.com/docs/gatsby-config/
- */
-
 module.exports = {
-  /* Your site config here */
-  plugins: [],
+  plugins: [
+    `gatsby-plugin-graphql-codegen`,
+    {
+      resolve: `gatsby-plugin-typescript`,
+      options: {
+        isTSX: true,
+        jsxPragma: `jsx`,
+        allExtensions: true,
+      },
+    }
+  ],
 }
