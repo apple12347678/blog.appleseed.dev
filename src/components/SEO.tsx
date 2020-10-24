430eec6c2cd3b5898d596e65b02e75c87d5a7f64
appleseed <apple12347678@gmail.com>
2020-10-24 16:14:09 +0900

    patch(SEO): add banner image for og meta

--- src/components/SEO.tsx
+++ src/components/SEO.tsx
@@ -50,6 +50,10 @@ export default function SEO({ title, meta = [] }: ISEOProps) {
       property: 'og:type',
       content: 'website',
     },
+    {
+      property: 'og:image',
+      content: '/static/banner.png',
+    },
   ];
 
   return (
