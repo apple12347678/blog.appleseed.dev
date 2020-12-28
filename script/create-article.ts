d95d7d64943bb0bb6ff1f7aef410945b79fa79da
appleseed <apple12347678@gmail.com>
2020-12-28 16:56:50 +0900

    patch(script): receive only slug for article cration

--- script/create-article.ts
+++ script/create-article.ts
@@ -4,22 +4,15 @@ import path from 'path';
 import yaml from 'yaml';
 import yargs from 'yargs/yargs';
 
-const { slug, title, description = '', tag: argvTag, lang = 'ko' } = yargs(
-  process.argv,
-).argv as {
+const { slug, lang = 'ko' } = yargs(process.argv).argv as {
   slug?: string;
-  title?: string;
-  description?: string;
-  tag?: string | string[];
   lang?: string;
 };
 
-if (!_.isString(slug) || !_.isString(title) || _.isUndefined(argvTag)) {
-  console.log('Usage: create --slug [slug] --title [title] --tag [tag] ...');
+if (!_.isString(slug)) {
+  console.log('Usage: create --slug [slug]');
   process.exit(1);
 }
-
-const tags = _.isString(argvTag) ? [argvTag] : argvTag;
 
 const newPostDirPath = path.resolve(
   path.dirname(__dirname),
@@ -31,10 +24,10 @@ const newPostDirPath = path.resolve(
 const newPostMarkdownPath = path.resolve(newPostDirPath, `${lang}.md`);
 
 const frontmatter = yaml.stringify({
-  title,
+  title: '',
   date: new Date(),
-  description,
-  tags,
+  description: '',
+  tags: [],
 });
 
 const newPostMarkdownContent = `---
